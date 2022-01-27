eda48de8af11144dc33c9f5704a7831bd61e2af0
deep-shikha124 <79909379+deep-shikha124@users.noreply.github.com>
2022-01-27 11:47:56 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (89.58%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string s;
+        for(auto i:address){
+            if (i=='.'){
+                s+="[.]";
+            }
+            else{
+                s+=i;
+            }
+        }
+        return s;
+    }
+};
