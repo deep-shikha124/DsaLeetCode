3738b429315ecd979ea1e763ff7c777faf0225dd
Deep Shikha <79909379+deep-shikha124@users.noreply.github.com>
2022-02-18 22:01:00 +0530

    Added solution - LeetHub

---
+++ 15. Mean And Median of Array - GFG/15.-mean-and-median-of-array.cpp
@@ -0,0 +1,67 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    //Function to find median of the array elements.
+    int median(int A[],int N)
+    {
+        int medi;
+        sort(A,A+N);
+        if(N%2==0){
+            medi=(A[N/2-1]+A[N/2])/2;
+        }
+        else{
+            medi=A[N/2];
+        }
+        return medi;
+        //your code here
+        //If median is in fraction then convert it to integer and return
+    }
+    //Function to find mean of the array elements.
+    int mean(int A[],int N)
+    {
+        //your code here
+        int sum=0;
+        for(int i=0;i<N;i++){
+            sum+=A[i];
+        }
+        return sum/N;
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main()
+{
+    //testcase
+    int T;
+    cin>>T;
+   
+    //looping testcase
+    while(T--)
+    {
+        //number of elements in array
+        int N;
+        cin>>N;
+        int a[N];
+        
+        //inseting elements in the array
+        for(int i=0;i<N;i++){
+            cin>>a[i];
+        }
+        Solution ob;
+        //calling mean() and median() functions
+        cout<<ob.mean(a,N)<<" "<<ob.median(a,N)<<endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
