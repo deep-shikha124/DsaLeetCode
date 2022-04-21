254f94477dc850cdd346c310140e42006a3f35bd
Deep Shikha <79909379+deep-shikha124@users.noreply.github.com>
2022-04-21 18:14:35 +0530

    Time: 106 ms (72.54%), Space: 46.5 MB (97.54%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -4,7 +4,6 @@ public:
         sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size()-1;i++){
             if(nums[i]==nums[i+1]) return true;
-            
         }return false;
     }
 };
