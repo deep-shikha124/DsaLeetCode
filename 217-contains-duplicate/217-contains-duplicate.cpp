f9dd7be7a4585e3d17954755e0ed66f5f64b82c6
Deep Shikha <79909379+deep-shikha124@users.noreply.github.com>
2022-05-31 19:20:39 +0530

    Time: 150 ms (28.75%), Space: 54.8 MB (6.38%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,9 +1,24 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1]) return true;
-        }return false;
+        // map<int,int>hash;
+        // for(int i=0;i<nums.size();i++){
+        //     if(hash.count(nums[i]!=0)){
+        //         return true;
+        //     }
+        //     else{
+        //         hash(nums[i])=1;
+        //     }
+        // }
+        // return false;
+        set <int> s;
+        for(int x:nums){
+            s.insert(x);
+        }
+        if(s.size()<nums.size()){
+            return true;
+        }
+        else return false;
+        
     }
 };
