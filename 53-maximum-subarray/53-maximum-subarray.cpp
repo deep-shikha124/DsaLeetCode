0e9d363a5a493428c7ad4320034a4ba4dbf9af33
Deep Shikha <79909379+deep-shikha124@users.noreply.github.com>
2022-06-10 22:37:45 +0530

    Time: 251 ms (5.12%), Space: 67.7 MB (90.87%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,7 +1,35 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        //brute force O(n3)--->
+//         int maxi=0;
+//         for(int i=0;i<nums.size()-1;i++){
+//             for(int j=i;j<nums.size()-1;j++){
+//                  int sum=0;
+//                 for(int k=i;k<j;k++){
+                   
+//                     sum=sum+nums[k];
+//                     if(sum>maxi)
+//                         maxi=sum;
+//                 }
+//             }
+//         }
+//         return maxi;
        
+        
+        //brute force O(n2)---->
+        // int maxi=0,sum;
+        // for(int i=0;i<nums.size()-1;i++){
+        //     sum=0;
+        //     for(int j=i;j<nums.size()-1;j++){
+        //         sum=sum+nums[j];
+        //         if(sum>maxi)
+        //             maxi=sum;
+        //     }
+        // }
+        // return maxi;
+        
+        //O(n)---->
         int n=nums.size();
         int sum=INT_MIN;
         int temp=0;        
@@ -12,5 +40,8 @@ public:
             sum=max(sum,temp);
         }
         return sum;
+        
+       
+        
     }
 };
