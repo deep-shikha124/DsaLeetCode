bcf7cb6a5758dfd3df1cce2599127d0254b4e249
Deep Shikha <79909379+deep-shikha124@users.noreply.github.com>
2022-06-11 18:43:11 +0530

    Time: 49 ms (56.67%), Space: 27.5 MB (64.09%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -10,5 +10,15 @@ public:
             else low = mid + 1;
         }
         return -1;
+        
+        // int n=nums.size();
+        // int low=0,high=n-1;
+        // int mid=low+(high-low)/2;
+        // while(low<=high){
+        //     if(nums[mid]==target) return mid;
+        //     else if(target>nums[mid]) low=mid+1;
+        //     else high=mid-1;
+        // }
+        // return -1;
     }
 };
