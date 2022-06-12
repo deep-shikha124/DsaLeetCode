3eee2d31b269d989473926a3f487c5c4da0b7779
Deep Shikha <79909379+deep-shikha124@users.noreply.github.com>
2022-06-13 03:19:26 +0530

    Time: 5 ms (25.18%), Space: 8.2 MB (90.56%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int cnt0=0,cnt1=0,cnt2=0;
-      for(int i=0;i<nums.size();i++){
-        if(nums[i]==0) cnt0++;
-        else if(nums[i]==1) cnt1++;
-        else cnt2++;
-      }
-      for(int i=0;i<cnt0;i++){
-        nums[i]=0;
-      }
-      for(int i=cnt0;i<cnt0+cnt1;i++){
-        nums[i]=1;
-      }
-      for(int i=cnt0+cnt1;i<nums.size();i++){
-        nums[i]=2;
+//         int cnt0=0,cnt1=0,cnt2=0;
+//       for(int i=0;i<nums.size();i++){
+//         if(nums[i]==0) cnt0++;
+//         else if(nums[i]==1) cnt1++;
+//         else cnt2++;
+//       }
+//       for(int i=0;i<cnt0;i++){
+//         nums[i]=0;
+//       }
+//       for(int i=cnt0;i<cnt0+cnt1;i++){
+//         nums[i]=1;
+//       }
+//       for(int i=cnt0+cnt1;i<nums.size();i++){
+//         nums[i]=2;
+//       }
+      
+      int low=0,mid=0,high=nums.size()-1;
+      while(mid<=high){
+        if(nums[mid]==1) mid++;
+        else if(nums[mid]==0) swap(nums[mid++],nums[low++]);
+        else if(nums[mid]==2) swap(nums[mid],nums[high--]);
       }
       
       }
