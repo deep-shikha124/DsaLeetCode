3173a1489cac34417760813c419fca3a585a15f3
Deep Shikha <79909379+deep-shikha124@users.noreply.github.com>
2022-06-25 18:48:20 +0530

    Time: 11 ms (39.23%), Space: 10.6 MB (37.74%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
+  
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>v;
+        vector<int>ans;
         map<int,int>m;
         for(int i=0;i<nums1.size();i++){
             m[nums1[i]]++;
@@ -9,9 +10,9 @@ public:
         for(int i=0;i<nums2.size();i++){
             if(m[nums2[i]]>0){
                 m[nums2[i]]--;
-                v.push_back(nums2[i]);
+                ans.push_back(nums2[i]);
             }
         }
-        return v;
+      return ans;
     }
 };
