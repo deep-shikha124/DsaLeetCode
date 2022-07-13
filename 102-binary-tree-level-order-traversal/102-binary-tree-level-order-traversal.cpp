5e8170a1f5937739c1ef250b1e360ba6710ca3d0
Deep Shikha <79909379+deep-shikha124@users.noreply.github.com>
2022-07-13 06:16:33 +0530

    Time: 7 ms (54.38%), Space: 13.5 MB (9.87%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
@@ -11,31 +11,31 @@
  */
 class Solution {
 public:
-    // vector<vector<int>>ans;
-    // void levelorder(TreeNode* root,int level){
-    //     if(!root) return;
-    //     if(level==ans.size()) ans.push_back({});
-    //     ans[level].push_back(root->val);
-    //     levelorder(root->left,level+1);
-    //     levelorder(root->right,level+1);
-    // }
-    // vector<vector<int>> levelOrder(TreeNode* root) {
-    //     vector<vector<int>>ans;
-    //     levelorder(root,0);
-    //     return ans;
-    vector<vector<int>> res;
-    void rec(TreeNode* root, int level) {
-        if (!root) return;
-        if (level == res.size()) res.push_back({});
-        res[level].push_back(root->val);
-        rec(root->left, level+1);
-        rec(root->right, level+1);
+    vector<vector<int>>ans;
+    void levelorder(TreeNode* root,int level){
+        if(!root) return;
+        if(level==ans.size()) ans.push_back({});
+        ans[level].push_back(root->val);
+        levelorder(root->left,level+1);
+        levelorder(root->right,level+1);
     }
-	
     vector<vector<int>> levelOrder(TreeNode* root) {
-        rec(root, 0);
-        return res;
-    }
+    
+        levelorder(root,0);
+        return ans;}
+//     vector<vector<int>> res;
+//     void rec(TreeNode* root, int level) {
+//         if (!root) return;
+//         if (level == res.size()) res.push_back({});
+//         res[level].push_back(root->val);
+//         rec(root->left, level+1);
+//         rec(root->right, level+1);
+//     }
+	
+//     vector<vector<int>> levelOrder(TreeNode* root) {
+//         rec(root, 0);
+//         return res;
+//     }
     
 
     
